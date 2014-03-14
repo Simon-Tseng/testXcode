6e1db81df3a09dbefa586e3e01900a8f8fa58840
Simon Tseng <nomis5888@gmail.com>
2014-03-14 14:58:11 +0800

    Add STBattery.h.m

---
+++ testGithubWithXcode/STBattery.h
@@ -0,0 +1,13 @@
+//
+//  STBattery.h
+//  testGithubWithXcode
+//
+//  Created by Simon Tseng on 14/3/14.
+//  Copyright (c) 2014年 Simon Tseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STBattery : UIView
+
+@end
---
+++ testGithubWithXcode/STBattery.m
@@ -0,0 +1,31 @@
+//
+//  STBattery.m
+//  testGithubWithXcode
+//
+//  Created by Simon Tseng on 14/3/14.
+//  Copyright (c) 2014年 Simon Tseng. All rights reserved.
+//
+
+#import "STBattery.h"
+
+@implementation STBattery
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
